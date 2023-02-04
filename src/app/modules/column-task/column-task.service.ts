992649f6ef5bf023cb45fd0611e19621eb2f820a
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-02-04 17:07:06 +0300

    feat: add methods in service for column component

--- src/app/modules/column-task/column-task.service.ts
+++ src/app/modules/column-task/column-task.service.ts
@@ -1,13 +1,47 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+interface IColumn {
+  nameColumn:string,
+  descriptionColumn:string
+}
+
+interface IColumnMove {
+  toBoardId: string,
+  newPosition: number
+}
+
 @Injectable()
+
 export class ColumnTaskService {
-  private url: string = 'http://localhost:3000/api/';
+  private urlColumn: string = 'http://localhost:3000/api/column';
+  private urlTask: string = 'http://localhost:3000/api/task';
 
   constructor(private httpClient: HttpClient) { }
 
   public getColumns(idBoard: string) {
-    return this.httpClient.get(`${this.url}column/${idBoard}`);
+    return this.httpClient.get(`${this.urlColumn}/${idBoard}`);
   }
+
+  public getColumnById(idColumn: string) {
+    return this.httpClient.get(`${this.urlColumn}/id/${idColumn}`);
+  }
+
+  public createColumn(idBoard:string,body:IColumn) {
+    return this.httpClient.post(`${this.urlColumn}/${idBoard}`,body);
+  }
+
+  public deleteColumn(idColumn:string) {
+    return this.httpClient.delete(`${this.urlColumn}/${idColumn}`);
+  }
+
+  public updateColumn(idColumn:string, body:IColumn){
+    return this.httpClient.put(`${this.urlColumn}/${idColumn}`,body);
+  }
+
+  public moveColumn(idColumn:string, body:IColumnMove){
+    return this.httpClient.put(`${this.urlColumn}/move/${idColumn}`,body);
+  }
+
+
 }
--- src/app/modules/column-task/component/column/column.component.ts
+++ src/app/modules/column-task/component/column/column.component.ts
@@ -13,5 +13,38 @@ export class ColumnComponent implements OnInit {
   ngOnInit(){
     this.ColumnTaskService.getColumns('9e67fcee-8b69-40cd-a335-5c506655cf9c')
     .subscribe((res) => {console.log(res)});
+
+    // this.ColumnTaskService.getColumnById('082ad0bd-700f-4a80-9106-d57ccbe66424')
+    // .subscribe((res) => {console.log(res)});
+
+    // this.ColumnTaskService.createColumn('9e67fcee-8b69-40cd-a335-5c506655cf9c',{
+    //   nameColumn: 'new Column...',
+    //   descriptionColumn: 'new Description...'
+    // }).subscribe((res) => {console.log(res)});
+
+    // this.ColumnTaskService.deleteColumn('ecba094f-3aa7-4927-9ba0-bf4723d427d6')
+    // .subscribe();
+
+    // this.ColumnTaskService.updateColumn('222f177e-b179-45fc-b8a7-a46fd10e0826',{
+    //   nameColumn: 'updating name...',
+    //   descriptionColumn:'updating description...'
+    // }).subscribe(res=>{
+    //   console.log(res);
+    //   this.ColumnTaskService.getColumns('9e67fcee-8b69-40cd-a335-5c506655cf9c')
+    //   .subscribe((res) => {console.log(res)});
+    // })
+
+    // this.ColumnTaskService.moveColumn('1322df7e-c4c7-4ff9-a63e-b193851b5dac',{
+    //   toBoardId:'9e67fcee-8b69-40cd-a335-5c506655cf9c',
+    //   newPosition: 0
+    // }).subscribe(res=>{
+    //   console.log(res);
+    //   this.ColumnTaskService.getColumns('9e67fcee-8b69-40cd-a335-5c506655cf9c')
+    //   .subscribe((res) => { console.log(res) });
+    // })
+
+    // this.ColumnTaskService.getTasks('9e67fcee-8b69-40cd-a335-5c506655cf9c','082ad0bd-700f-4a80-9106-d57ccbe66424')
+    // .subscribe(res=>{console.log(res)});
+
   }
 }
