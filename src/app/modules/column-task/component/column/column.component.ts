10613b38456d1c5e78efdceaaeffc63772ae1aaa
Stanislav Mamulin <StanislavPM@ya.ru>
2023-02-05 19:48:13 +0500

    feat: add move task between columns

--- src/app/modules/column-task/component/column/column.component.ts
+++ src/app/modules/column-task/component/column/column.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { ColumnTaskService } from '../../column-task.service';
 import { IColumn } from '../../model/column.interface';
 import { ITask } from '../../model/task.interface';
@@ -16,5 +17,18 @@ export class ColumnComponent implements OnInit {
 
   ngOnInit() {
     this.tasks = this.column.tasks;
+  }
+
+  drop(event: CdkDragDrop<ITask[]>) {
+     if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 }
