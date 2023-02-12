2ecb78cccee1616a690ad081be0e8207d6b53b10
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-02-12 18:03:01 +0300

    feat: add guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './shared/auth.guard';
 
 const routes: Routes = [
   {
@@ -14,6 +15,7 @@ const routes: Routes = [
         path: 'board',
         loadChildren: () =>
           import('./pages/board-page/board-page.module').then((page) => page.BoardPageModule),
+        canActivate:[AuthGuard]
       },
       {
         path: 'main',
