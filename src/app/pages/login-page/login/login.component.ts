c3a381b0330dcf2fe82df3385ad2d0166ef179dd
Stanislav Mamulin <StanislavPM@ya.ru>
2023-02-12 13:31:35 +0500

    feat: add submit login data to server

--- src/app/pages/login-page/login/login.component.ts
+++ src/app/pages/login-page/login/login.component.ts
@@ -1,19 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { LoginParams } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  form:any;
-  isHidePassword:boolean = true;
+  form: FormGroup;
+
+  isHidePassword = true;
+
+  submitted = false;
+
+  constructor(private auth: AuthService) {}
 
   ngOnInit(): void {
     this.form = new FormGroup({
       emailFormControl: new FormControl('', [Validators.required, Validators.email]),
-      enterPassword: new FormControl('',[Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/)]),
-    })
+      enterPassword: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/)]),
+    });
+  }
+
+  submit() {
+    if (this.form.invalid) return;
+
+    this.submitted = true;
+
+    const loginData: LoginParams = {
+      email: this.form.value.emailFormControl,
+      password: this.form.value.enterPassword,
+    };
+
+    this.auth.login(loginData).subscribe((res) => console.log(res));
   }
 }
