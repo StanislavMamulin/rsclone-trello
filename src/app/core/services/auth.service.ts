42de942597624964d411135b589663aa4527d78c
Stanislav Mamulin <StanislavPM@ya.ru>
2023-02-12 15:59:12 +0500

    feat: add methods to store token and working with it

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { IUser, LoginParams } from 'src/app/shared/models/user.model';
+import { Observable, tap } from 'rxjs';
+import { LoginParams, UserResponse } from 'src/app/shared/models/user.model';
 import { BASE_URL } from '../constants';
+
+const TOKEN_EXP_KEY = 'token-exp';
+const TOKEN_KEY = 'token';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +15,46 @@ export class AuthService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public login(loginParams: LoginParams): Observable<IUser> {
-    return this.httpClient.post<IUser>(this.loginUrl, loginParams);
+  public login(loginParams: LoginParams): Observable<UserResponse > {
+    return this.httpClient.post<UserResponse>(this.loginUrl, loginParams)
+      .pipe(
+        tap(this.setToken),
+      );
+  }
+
+  public logout() {
+    this.resetToken();
+  }
+
+  private setToken(response: UserResponse) {
+    const { expiresIn, token } = response;
+
+    localStorage.setItem(TOKEN_EXP_KEY, new Date(expiresIn).toString());
+    if (token) localStorage.setItem('token', token);
+      
+  }
+
+  private resetToken() {
+    localStorage.removeItem(TOKEN_EXP_KEY);
+    localStorage.removeItem(TOKEN_KEY);
+  }
+
+  get token() {
+    const existExpDate = localStorage.getItem(TOKEN_EXP_KEY);
+    if (existExpDate) {
+      const expDate = new Date(existExpDate);
+      if (new Date() > expDate) {
+        this.logout();
+        return null;
+      }
+
+      return localStorage.getItem(TOKEN_KEY);
+    }
+
+    return null;
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.token;
   }
 }
