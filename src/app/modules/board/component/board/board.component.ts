a0cd0ba4d063494a2422b2a21e2b878a35bd012a
Nebesnaya94 <kuroi.alice.13@gmail.com>
2023-02-27 04:57:55 +0300

    fix: edit open board audio, add delete board audio

--- src/app/modules/board/component/board/board.component.ts
+++ src/app/modules/board/component/board/board.component.ts
@@ -40,6 +40,7 @@ export class BoardComponent implements OnInit {
   }
 
   deleteBoard(id: string) {
+    this.audioService.playAudio('../../../../assets/sounds/audio-delete.mp3');
     this.onDelete.emit(id);
   }
 
--- src/app/shared/audio-service.service.ts
+++ src/app/shared/audio-service.service.ts
@@ -6,22 +6,24 @@ import { AppStateService } from '../core/services/app-state.service';
   providedIn: 'root',
 })
 export class AudioServiceService {
-  isSoundEnable:boolean = true;
+  isSoundEnable: boolean = true;
   subscription: Subscription;
 
-  constructor(
-    private appStateService: AppStateService
-  ) {
-    this.appStateService.isSoundEnable$.subscribe((enableSound)=>{
+  constructor(private appStateService: AppStateService) {
+    this.appStateService.isSoundEnable$.subscribe((enableSound) => {
       this.isSoundEnable = enableSound;
-    })
+    });
   }
 
   playAudio(audioLink: string) {
-    if(this.isSoundEnable){
+    if (this.isSoundEnable) {
       const audio = new Audio();
       audio.src = audioLink;
-      audio.play();
+      audio.ondurationchange = () => {
+        console.log(audio.duration);
+        console.log(audio.currentTime);
+        audio.duration ? audio.play() : console.log('no');
+      };
     }
   }
 }
